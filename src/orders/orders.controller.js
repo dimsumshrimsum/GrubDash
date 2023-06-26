8ddba0579eb184f334f6fdafcb5e8ac0e93d79b3
Zach Robles <zrobles1188@gmail.com>
2023-06-26 16:21:33 -0400

    working get by order id

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,22 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
+function validateOrderExists(req, res, next) {
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === orderId);
+  if (index < 0) {
+    next({ status: 404, message: "order not found" });
+  } else {
+    res.locals.index = index;
+    res.locals.order = orders[index];
+    next();
+  }
+}
+
+function read(req, res) {
+  res.send({ data: res.locals.order });
+}
+
+module.exports = { read: [validateOrderExists, read] };
+
 // TODO: Implement the /orders handlers needed to make the tests pass
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-
+const controller = require("./orders.controller");
 // TODO: Implement the /orders routes needed to make the tests pass
+
+router.route("/:orderId").get(controller.read);
 
 module.exports = router;
